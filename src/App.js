5586e5a497789c73c8d70a52d9570dd910537c5c
unknown <roshinandalt275@gmail.com>
2023-06-15 17:25:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './styles/styles.css';
 
-function App() {
+function ProfileCard({ user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="profile-card">
+      <h2 className="username">{user.username}</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>Website: {user.website} </p>
     </div>
   );
 }
 
-export default App;
+
+export default function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users/1'); 
+        setData(response.data);
+        setIsError(false);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 style={{ textAlign: 'center' }}>1</h1>
+        {isLoading ? (
+          <p>WAITING Dear...</p>
+        ) : isError ? (
+          <p>Error occurred Poyi API sheriyakkade.</p>
+        ) : (
+          <>
+            {data && <ProfileCard user={data} />}
+            <div className="button-container">
+              <button>-</button>
+              <button>+</button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+     }
